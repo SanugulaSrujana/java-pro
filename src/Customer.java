5c1fb58ff9f6a805831aaa77fa281bcc8ffa16c4
SanugulaSrujana <sanugulasrujana@gmail.com>
2024-08-12 15:46:15 +0530

    updating and adding file

---
+++ src/Customer.java
@@ -0,0 +1,64 @@
+package day5;
+
+import java.util.TreeMap;
+
+public class Customer implements Comparable<Customer> 
+{
+	static TreeMap<Integer,String> details=new TreeMap<>();;
+	int customerId;
+	String name;
+	String location;
+	String email;
+	Customer()
+	{
+		this.customerId=101;
+		this.setName("nandu");
+		this.setLocation("Bangalore");
+		this.email="xyz@gamil.com";
+		String det=this.getName()+"\t"+this.email+"\t"+this.getLocation();
+		details.put(this.customerId,det);
+	}
+	Customer(int customerId,String name,String location,String email)
+	{
+		this.customerId=customerId;
+		this.setName(name);
+		this.setLocation(location);
+		this.email=email;
+		String det=this.getName()+"\t"+this.email+"\t"+this.getLocation();
+		details.put(this.customerId,det);
+	}
+	
+	
+	@Override
+	public int compareTo(Customer customer) {
+		return this.getName().compareTo(customer.getName());
+	}
+	
+	public int getCustomerId()
+	{
+		return this.customerId;
+	}
+	
+	public String toString()
+	{
+		return this.customerId+"\t"+this.email+"\t"+this.getLocation()+"\t"+this.getName();
+	}
+	public void setCustomerId(int id)
+	{
+		this.customerId=id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+
+}
